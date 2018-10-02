486c47a559122f8b19e5b519ddb3dcfcba187ac6
jessicaseto <jes.seto@gmail.com>
2018-10-02 13:33:13 -0700

    add location and speed to enemy class

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 // Enemies our player must avoid
 class Enemy {
     constructor (sprite) {
-        // Variables applied to each of our instances go here,
-        // we've provided one for you to get started
-
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
+
+        // Location information
+        this.x = 0;
+        this.y = getRndInteger(2, 4) * 83;
+
+        // Speed
+        this.speed = 1;
     }
 
     // Update the enemy's position, required method for game
@@ -15,6 +19,7 @@ class Enemy {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
+        this.x += (this.speed * dt);
     };
 
     // Draw the enemy on the screen, required method for game
@@ -24,12 +29,19 @@ class Enemy {
 
 };
 
+// Random integer function
+// From: https://www.w3schools.com/js/js_random.asp
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
 // Now instantiate your objects.
+
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
